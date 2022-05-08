3e2bdea060f5b656c573195b10f893f7c0c07e62
Klinton <chhunklinton91@gmail.com>
2022-05-08 22:13:10 +0700

    update check vote status by validate national id

--- src/controller/vote/status.ts
+++ src/controller/vote/status.ts
@@ -1,8 +1,14 @@
 import VoterModel from '../../models/voter';
+import { isNationalID } from '../../utils';
 
 const status = async (req: any, res: any) => {
   try {
     const { nationalId } = req.body;
+
+    if (!isNationalID(nationalId)) {
+      return res.status(200).json({ status: false });
+    }
+
     const voter = await VoterModel.findOne({ nationalId });
 
     return res.status(200).json({ status: voter === null });
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,14 @@ import ElectionStatusModel from '../models/electionStatus';
 
 export const getElectionStatus = async () =>
   ElectionStatusModel.findOne().select({ enable: 1 }).exec();
+
+export const isNationalID = (id: string): boolean => {
+  let [i, sum] = [0, 0];
+
+  for (i = 0; i < 12; i++) {
+    sum += parseInt(id.charAt(i)) * (13 - i);
+  }
+  const check = (11 - (sum % 11)) % 10;
+
+  return check === parseInt(id.charAt(12));
+};
